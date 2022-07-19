9a4976278d4f99e70ced02da159d2ae1212e6b6a
107Neel <20IT071@charusat.edu.in>
2022-07-19 23:00:52 +0530

    image-slider commit

---
+++ src/Components/Slider/Slider.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react'
+import './Icons/Slider.css'
+import BtnSlider from './Icons/BtnSlider'
+import dataslider from './Icons/dataslider'
+
+export default function Slider() {
+
+    const [slideIndex, setSlideIndex] = useState(1)
+
+    const nextSlide = () => {
+        if(slideIndex !== dataslider.length){
+            setSlideIndex(slideIndex + 1)
+        } 
+        else if (slideIndex === dataslider.length){
+            setSlideIndex(1)
+        }
+    }
+
+    const prevSlide = () => {
+        if(slideIndex !== 1){
+            setSlideIndex(slideIndex - 1)
+        }
+        else if (slideIndex === 1){
+            setSlideIndex(dataslider.length)
+        }
+    }
+
+    const moveDot = index => {
+        setSlideIndex(index)
+    }
+
+    return (
+        <div className="container-slider">
+            {dataslider.map((obj, index) => {
+                return (
+                    <div
+                    key={obj.id}
+                    className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
+                    >
+                        <img 
+                        src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`} 
+                        />
+                    </div>
+                )
+            })}
+            <BtnSlider moveSlide={nextSlide} direction={"next"} />
+            <BtnSlider moveSlide={prevSlide} direction={"prev"}/>
+
+            <div className="container-dots">
+                {Array.from({length: 5}).map((item, index) => (
+                    <div 
+                    onClick={() => moveDot(index + 1)}
+                    className={slideIndex === index + 1 ? "dot active" : "dot"}
+                    ></div>
+                ))}
+            </div>
+        </div>
+    )
+}
