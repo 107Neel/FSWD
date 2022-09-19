3a96ca0ecdcd3b0166a083f1af21f31f272f2dc9
107Neel <20IT071@charusat.edu.in>
2022-09-19 14:19:17 +0530

    complete first task of node

---
+++ input.js
@@ -0,0 +1,11 @@
+const {add,sub,multi,div}=require('./calculator')
+
+console.log("Hello World")
+
+console.log(add(1,2));
+
+console.log(sub(1,2));
+
+console.log(multi(1,2));
+
+console.log(div(1,2));
