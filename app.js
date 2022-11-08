ee09627bd6a59f4f9da6d86aea4fdab80d31840b
107Neel <20IT071@charusat.edu.in>
2022-11-08 19:55:02 +0530

    prac 2

---
+++ app.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const dbconn = require("./dbconnect");
+const httpServer = http.createServer(function (req, res) {
+  if (req.url === "/listStudents") {
+    if (dbconn == "connected");
+    {
+      console.log("Connected");
+      q = "SELECT * FROM student limit 1";
+      dbconn.connection.query(q, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
+    }
+    res.end("List of students");
+  } else if (req.url === "/updateStudent") {
+    if (dbconn == "connected");
+    {
+      console.log("Connected");
+      q = "UPDATE name FROM student where ID=50";
+      dbconn.connection.query(q, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
+    }
+    res.end("updating student");
+  } else if (req.url === "/deleteStudent") {
+    if (dbconn == "connected");
+    {
+      console.log("Connected");
+      q = "DELETE * FROM student where ID=50";
+      dbconn.connection.query(q, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
+    }
+    res.end(" Deleting Student");
+  } else {
+    res.end("Improper request");
+  }
+});
---
+++ dbconnect.js
@@ -0,0 +1,14 @@
+var mysql = require("mysql");
+var con = mysql
+  .createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "student",
+  })
+  .connect(function (err) {
+    if (err) return err;
+    return "Connected";
+  });
+console.log(con);
+module.exports.connection = con;
